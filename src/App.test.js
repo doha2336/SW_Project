12e9b28a8a97bad77dd9251788306a0375b49abd
Doha <d.waleed2336@nu.edu.eg>
2025-12-03 23:07:13 +0200

    Full mock react-router-dom for tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,16 @@
-
-jest.mock('react-router-dom', () => ({
-  useNavigate: () => jest.fn(),
-  BrowserRouter: ({ children }) => <>{children}</>,
-  Routes: ({ children }) => <>{children}</>,
-  Route: ({ children }) => <>{children}</>,
-  Link: ({ children }) => <>{children}</>,
-}));
-
-
+// Full mock for react-router-dom to make Jest tests pass
+jest.mock('react-router-dom', () => {
+  const React = require('react');
+  return {
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => jest.fn(),
+    BrowserRouter: ({ children }) => <>{children}</>,
+    Routes: ({ children }) => <>{children}</>,
+    Route: ({ children }) => <>{children}</>,
+    Link: ({ children }) => <>{children}</>,
+    Outlet: () => <div />,
+  };
+});
 
 import { render, screen } from '@testing-library/react';
 import App from './App';
