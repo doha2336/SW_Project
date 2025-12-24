0007234978c327e5f3727cc2461c6450653b789d
Doha <d.waleed2336@nu.edu.eg>
2025-12-24 04:09:48 +0200

    Remove The_project_back folder from Backup_Frontend branch

--- The_project_back/run_backend.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting Django Backend Server...
-cd /d "C:\Users\LOQ\Desktop\project (2)\project\The project back"
-python manage.py runserver 0.0.0.0:8000
-pause
--- The_project_back/user_messages/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserMessagesConfig(AppConfig):
-    name = 'user_messages'
