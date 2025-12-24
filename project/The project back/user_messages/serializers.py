3f9cdab2ee6a3bb8379f7ac9b5ef109fddb3a08f
Doha <d.waleed2336@nu.edu.eg>
2025-12-24 03:15:51 +0200

    Clear main branch

--- project/The project back/user_messages/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import UserMessage
-
-
-class MessageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserMessage
-        fields = '__all__'
-        read_only_fields = ['sender']
--- project/The project front/run_frontend.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting Frontend Development Server...
-cd /d "C:\Users\LOQ\Desktop\project (2)\project\The project front"
-npm run dev
-pause
